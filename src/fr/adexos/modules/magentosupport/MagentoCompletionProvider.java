f9c23f6aa19c569d82281f7838d24a77bb850485
Loic <Loic@PC2015-02>
2015-04-27 11:46:41 +0200

    Commit before pushing to github

--- src/fr/adexos/modules/magentosupport/MagentoCompletionProvider.java
+++ src/fr/adexos/modules/magentosupport/MagentoCompletionProvider.java
@@ -1,5 +1,8 @@
 package fr.adexos.modules.magentosupport;
 
+import fr.adexos.modules.magentosupport.Magento.MagentoModel;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
@@ -21,6 +24,19 @@ import org.openide.util.Exceptions;
 @MimeRegistration(mimeType = "text/x-php5", service = CompletionProvider.class)
 public class MagentoCompletionProvider implements CompletionProvider {
 
+    
+    /**
+     * On startup, build the main collection of Magento classes
+     */
+    public MagentoCompletionProvider() {
+        // Models
+        HashMap MageModels = new HashMap<String, MagentoModel>();
+        MageModels.put("catalog/product", new MagentoModel("catalog/product"));
+        
+        // Helpers
+        
+    }
+
     @Override
     public CompletionTask createTask(int queryType, JTextComponent jtc) {
 
@@ -29,6 +45,7 @@ public class MagentoCompletionProvider implements CompletionProvider {
         }
 
         return new AsyncCompletionTask(new AsyncCompletionQuery() {
+            @Override
             protected void query(CompletionResultSet completionResultSet, Document document, int caretOffset) {
                 String filter = null;
                 int startOffset = caretOffset - 1;
@@ -39,6 +56,13 @@ public class MagentoCompletionProvider implements CompletionProvider {
                     final char[] line = bDoc.getText(lineStartOffset, caretOffset - lineStartOffset).toCharArray();
                     final int whiteOffset = indexOfWhite(line);
                     filter = new String(line, whiteOffset + 1, line.length - whiteOffset - 1);
+                    // Filter out all non Magento stuff
+                    if (!filter.startsWith("Mage::")) {
+                        System.out.println("MageSupport : Text is not related");
+                        completionResultSet.finish();
+                        return;
+                    }
+                    System.out.println(filter);
                     if (whiteOffset > 0) {
                         startOffset = lineStartOffset + whiteOffset + 1;
                     } else {
@@ -48,23 +72,19 @@ public class MagentoCompletionProvider implements CompletionProvider {
                     Exceptions.printStackTrace(ex);
                 }
 
-                //Iterate through the available locales
-                //and assign each country display name
-                //to a CompletionResultSet:
-                Locale[] locales = Locale.getAvailableLocales();
-                for (int i = 0; i < locales.length; i++) {
-                    final Locale locale = locales[i];
-                    final String country = locale.getDisplayCountry();
-                    //Here we test whether the country starts with the filter defined above:
-                    if (!country.equals("") && country.startsWith(filter)) {
-                        //Here we include the start offset, so that we'll be able to figure out
-                        //the number of characters that we'll need to remove:
-                        completionResultSet.addItem(new MageCompletionItem(country, startOffset, caretOffset));
-                    }
+                if (filter.matches("/Mage::getModel/")) {
+                    // Find DocBlock
+                    
+                    completionResultSet.addItem(new MageCompletionItem("", startOffset, caretOffset));
+                } else if (filter.matches("/Mage::helper/")) {
+                    
+                } else {
+                    System.out.println("MageSupport : Nothing to do");
                 }
+                
                 completionResultSet.finish();
             }
-        });
+        }, jtc);
 
     }
 
